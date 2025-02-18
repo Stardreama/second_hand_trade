ad67653bc2a0e207d8d46b1cad44544e59161bf2
Kisara <214858752@qq.com>
2025-02-18 12:39:11 +0800

    fix:detect id is different

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,22 @@
 const jwtService = require("../services/jwtService");
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
+
 const register = (req, res) => {
   const { student_id, username, password, student_card } = req.body;
 
+  // 首先检查 student_id 是否已存在
+  User.findByStudentId(student_id, (err, result) => {
+    if (err) {
+      return res.status(500).json({ message: "数据库错误" });
+    }
+
+    if (result.length > 0) {
+      // 如果 student_id 已存在，返回错误信息
+      return res.status(400).json({ message: "该学生ID已被注册" });
+    }
+  });
+  // 创建新用户
   User.create(student_id, username, password, student_card, (err, result) => {
     if (err) {
       return res.status(500).json({ message: "数据库错误" });
--- models/user.js
+++ models/user.js
@@ -1,11 +1,20 @@
-const mysql = require("mysql2");
-const { db, jwt } = require("../config/config");
+const { db } = require("../config/config");
 
 const User = {
+  // 创建用户
   create: (student_id, username, password, student_card, callback) => {
     const query =
       "INSERT INTO users (student_id, username, password, student_card) VALUES (?, ?, ?, ?)";
     db.query(query, [student_id, username, password, student_card], callback);
+  },
+
+  // 根据 student_id 查询用户
+  findByStudentId: (student_id, callback) => {
+    const query = "SELECT * FROM users WHERE student_id = ?";
+    db.query(query, [student_id], (err, result) => {
+      if (err) return callback(err, null);
+      callback(null, result); // 返回查询结果
+    });
   },
 };
 
