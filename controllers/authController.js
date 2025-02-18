eec2ead62e9e7545f981de01d4af7c6d505eb24f
Kisara <214858752@qq.com>
2025-02-18 14:05:25 +0800

    feat:Added login functionality and completed hash encryption related content

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ const jwtService = require("../services/jwtService");
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
 
+//注册
 const register = (req, res) => {
   const { student_id, username, password, student_card } = req.body;
 
@@ -15,15 +16,51 @@ const register = (req, res) => {
       // 如果 student_id 已存在，返回错误信息
       return res.status(400).json({ message: "该学生ID已被注册" });
     }
-  });
-  // 创建新用户
-  User.create(student_id, username, password, student_card, (err, result) => {
-    if (err) {
-      return res.status(500).json({ message: "数据库错误" });
-    }
-
-    res.status(200).json({ message: "注册成功" });
+    // 密码加密
+    bcrypt.hash(password, 10, (err, hashedPassword) => {
+      if (err) {
+        return res.status(500).json({ message: "密码加密失败" });
+      }
+      // 创建新用户
+      User.create(
+        student_id,
+        username,
+        hashedPassword,
+        student_card,
+        (err, result) => {
+          if (err) {
+            return res.status(500).json({ message: "数据库错误" });
+          }
+          res.status(200).json({ message: "注册成功" });
+        }
+      );
+    });
   });
 };
 
-module.exports = { register };
+//登录
+const login = (req, res) => {
+  const { student_id, password } = req.body;
+
+  // 查询用户是否存在
+  User.findByStudentId(student_id, (err, user) => {
+    if (err || !user) {
+      return res.status(404).json({ message: "用户不存在" });
+    }
+
+    // 验证密码
+    bcrypt.compare(password, user.password, (err, match) => {
+      if (err || !match) {
+        return res.status(401).json({ message: "密码错误" });
+      }
+
+      // 登录成功，生成JWT
+      const token = jwtService.generateToken(user);
+
+      // 返回JWT
+      res.status(200).json({ token });
+    });
+  });
+};
+
+module.exports = { register, login };
