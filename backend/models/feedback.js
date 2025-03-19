5c932a449412f1985d8f7b90eedcc810f77ac9e2
aurora-boop <2158191903@qq.com>
2025-03-19 20:31:04 +0800

    完成意见反馈

---
+++ backend/models/feedback.js
@@ -0,0 +1,17 @@
+const config = require("../config/config");
+const db = config.db;
+
+const Feedback = {
+    create: (feedback, callback) => {
+        const query = "INSERT INTO feedback (user_id, issue_type, description, images, contact) VALUES (?, ?, ?, ?, ?)";
+        db.query(query, [
+            feedback.user_id,
+            feedback.issue_type,
+            feedback.description,
+            feedback.images,
+            feedback.contact
+        ], callback);
+    }
+};
+
+module.exports = Feedback;
